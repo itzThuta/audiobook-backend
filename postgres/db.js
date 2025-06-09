d9c49efb68c56977956e8b663747917d0a442a44
Thu Ta Zaw <thutazaw@Thus-MacBook-Pro.local>
2025-06-09 13:49:09 +0700

    4th commit

--- postgres/db.js
+++ postgres/db.js
@@ -1,26 +1,21 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Automatically enable SSL in production (like Render)
-const useSSL = process.env.NODE_ENV === "production";
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT,
     dialect: "postgres",
     logging: false,
-    dialectOptions: useSSL
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : {},
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       max: 5,
       min: 0,
@@ -30,7 +25,6 @@ const sequelize = new Sequelize(
   }
 );
 
-// Test DB connection
 sequelize
   .authenticate()
   .then(() => console.log("✅ Database connected successfully!"))
