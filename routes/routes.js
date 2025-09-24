718ac1215a338b442d03c54c4d4b9c1751d3a7f9
Thu Ta Zaw <kuku.zayn@gmail.com>
2025-09-24 10:32:16 +0700

    update user

--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.delete('/delete_users_byID/:id', verifyToken, deleteUser); 
-router.put('/update_users_byID/:id', verifyToken, updateUser); 
+router.put('/update_users_byID/:id', updateUser); 
 router.get('/get_all_users', getAllUsers);
 router.post('/create_books', createBook);  
 router.get('/get_all_books', getAllBooks); 
